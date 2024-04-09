8a4e12ee01c59a121a8520d69c746d2584ab2e18
LeQuangKhai-IT <khaiquangletn@gmail.com>
2024-04-09 15:40:51 +0700

    second commit

--- src/database/database.js
+++ src/database/database.js
@@ -4,7 +4,8 @@ const conn = mysql.createConnection({
   host: '127.0.0.1',
   user: 'root',
   password: '',
-  database: 'webcraw'
+  database: 'webcraw',
+  port:'3306'
 })
 
 const getConnection = () =>{
