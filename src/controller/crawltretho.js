8f84dd116c4d052ef62b05b26bc398878ab4de12
LeQuangKhai-IT <khaiquangletn@gmail.com>
2024-05-16 17:18:46 +0700

    update nhẹ

--- src/controller/crawltretho.js
+++ src/controller/crawltretho.js
@@ -10,26 +10,26 @@ async function getHTML() {
 }
 
 const crawTreTho = async () => {
-getHTML().then((res) => {
-    const listArticle = [];
-    const $ = cheerio.load(res);
-    $('.bg-white .mh-120x').each(function () {
-        const title = $(this).find('p').text();
-        const type = $(this).find('a.tw-text-xs.text-primary.tw-leading-4.one-line.tw-w-auto.!tw-inline-block.mw-180x').text();
-        const url2 = $(this).find('.flex__1 a').attr('href');
-        const time2 = $(this).find('div.text-gray-v4.tw-leading-4.tw-text-xs span:eq(3)').text().toString().replaceAll('\n', '').trim();
-        
-        if(title != "" && type != "" && url2 != "" && time2 != "" ){
-            const url = "https://www.webtretho.com" + url2
-            const time = time2.split("/").reverse().join("-");
-            listArticle.push({ title, type, url, time })
-        }      
-    });
-      listArticle.forEach((article,index) => {
-          updateItem("webtrethos", article , index);
-      })
-      console.log("Update trẻ thơ thành công!")
-})
+    getHTML().then((res) => {
+        const listArticle = [];
+        const $ = cheerio.load(res);
+        $('.bg-white .mh-120x').each(function () {
+            const title = $(this).find('p').text();
+            const type = $(this).find('a.tw-text-xs.text-primary.tw-leading-4.one-line.tw-w-auto.!tw-inline-block.mw-180x').text();
+            const url2 = $(this).find('.flex__1 a').attr('href');
+            const time2 = $(this).find('div.text-gray-v4.tw-leading-4.tw-text-xs span:eq(3)').text().toString().replaceAll('\n', '').trim();
+
+            if (title != "" && type != "" && url2 != "" && time2 != "") {
+                const url = "https://www.webtretho.com" + url2
+                const time = time2.split("/").reverse().join("-");
+                listArticle.push({ title, type, url, time })
+            }
+        });
+        listArticle.forEach((article, index) => {
+            updateItem("trethos", article, index);
+        })
+        console.log("Update trẻ thơ thành công!")
+    })
 }
 
 export default crawTreTho;
